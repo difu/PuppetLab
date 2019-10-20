f0c468349d3b1b28be0ede68a4b515ab3459425d
Dirk Fuchs <difu@mac.com>
2019-10-20 19:27:15 +0200

    reformat

--- terraform/05-instances.tf
+++ terraform/05-instances.tf
@@ -34,26 +34,26 @@ module "puppet-testlab-asgroup" {
   recreate_asg_when_lc_changes = true
   key_name                     = var.key_name
 
-//  ebs_block_device = [
-//    {
-//      device_name           = "/dev/xvdz"
-//      volume_type           = "gp2"
-//      volume_size           = "50"
-//      delete_on_termination = true
-//    },
-//  ]
-//
-//  root_block_device = [
-//    {
-//      volume_size           = "50"
-//      volume_type           = "gp2"
-//      delete_on_termination = true
-//    },
-//  ]
+  //  ebs_block_device = [
+  //    {
+  //      device_name           = "/dev/xvdz"
+  //      volume_type           = "gp2"
+  //      volume_size           = "50"
+  //      delete_on_termination = true
+  //    },
+  //  ]
+  //
+  //  root_block_device = [
+  //    {
+  //      volume_size           = "50"
+  //      volume_type           = "gp2"
+  //      delete_on_termination = true
+  //    },
+  //  ]
 
   # Auto scaling group
   asg_name                  = "puppet-nodes-asg"
-  vpc_zone_identifier       = [aws_subnet.public-a.id,aws_subnet.public-b.id,aws_subnet.public-c.id]
+  vpc_zone_identifier       = [aws_subnet.public-a.id, aws_subnet.public-b.id, aws_subnet.public-c.id]
   health_check_type         = "EC2"
   min_size                  = var.group_a_min_servers
   max_size                  = var.group_a_max_servers
@@ -70,12 +70,11 @@ module "puppet-testlab-asgroup" {
 
   tags_as_map = {
     instance_role = "webserver"
-    environment = "dev"
+    environment   = "dev"
   }
 }
 
 data "template_file" "puppet_master_init" {
-    template = file("user_data/puppet_master.sh")
   template = file("user_data/puppet_master.sh")
   vars = {
     internal_domain = var.dns_zone_name
@@ -83,15 +82,15 @@ data "template_file" "puppet_master_init" {
 }
 
 resource "aws_instance" "puppetmaster" {
-  ami = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
   associate_public_ip_address = "true"
-  subnet_id = aws_subnet.public-a.id
+  subnet_id                   = aws_subnet.public-a.id
   vpc_security_group_ids = [
     aws_security_group.puppet-public-ssh.id,
-    aws_security_group.puppet-public-puppet.id]
+  aws_security_group.puppet-public-puppet.id]
   user_data = base64encode(data.template_file.puppet_master_init.rendered)
-  key_name = var.key_name
+  key_name  = var.key_name
   tags = {
     Name = "Puppet master"
   }
