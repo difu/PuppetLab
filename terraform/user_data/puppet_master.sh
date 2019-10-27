c463316089dd42a89ff89f490aefac98560edb5f
Dirk Fuchs <difu@mac.com>
2019-10-27 11:20:48 +0100

    install pdk

--- terraform/user_data/puppet_master.sh
+++ terraform/user_data/puppet_master.sh
@@ -18,8 +18,10 @@ chmod 644 /etc/hosts
 yum -y remove java-1.7.0-openjdk -y
 
 rpm -Uvh http://yum.puppetlabs.com/puppet-release-el-6.noarch.rpm
+rpm -Uvh https://yum.puppet.com/puppet-tools-release-el-6.noarch.rpm
 
 yum -y install puppetserver
+yum -y install pdk
 
 /opt/puppetlabs/bin/puppet config set dns_alt_names "puppetmaster.difu.internal,puppetmaster" --section master
 /opt/puppetlabs/bin/puppet config set certname puppetmaster.difu.internal
