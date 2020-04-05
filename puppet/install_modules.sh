e46dd9f14de94a4291236d8d61bac4b1b14c2f69
Dirk Fuchs <difu@mac.com>
2020-04-05 17:41:17 +0200

    Added module for apt

--- puppet/install_modules.sh
+++ puppet/install_modules.sh
@@ -1,2 +1,3 @@
 /opt/puppetlabs/bin/puppet module install puppetlabs-apache --environment $(cat /environment)
 /opt/puppetlabs/bin/puppet module install puppetlabs-postgresql --environment $(cat /environment)
+/opt/puppetlabs/bin/puppet module install puppetlabs-apt --version 7.4.1 --environment $(cat /environment)
